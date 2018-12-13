3b81d9b7ceda2dbf2f2a76feff7300a2d5cc7b94
Chris Hannam <chris.hannam@babylonhealth.com>
2018-12-13 12:07:25 +0000

    latest changes

---
+++ multi_layer_runner.py
@@ -0,0 +1,42 @@
+from keras import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras import backend as K
+
+SIZE = 64, 64
+
+
+class SmallerVGGNet:
+    @staticmethod
+    def build(width, height, depth, classes, finalAct="softmax"):
+        # initialize the model along with the input shape to be
+        # "channels last" and the channels dimension itself
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        # if we are using "channels first", update the input shape
+        # and channels dimension
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+
+def resize(image):
+    return image.resize(size=SIZE)
+
+
+def main():
+    vgg_16 = VGG16()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
